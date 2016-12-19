314c95b0c8daff212e00a975be9ca4f1b066327a
Bart Kastermans <kasterma@kasterma.net>
2016-12-19 19:38:31 +0100

    i2a and a2i written

--- R/factors.R
+++ R/factors.R
@@ -1,31 +1,50 @@
-#' Title
+#' Convert indices to assignments
 #'
-#' @param idx
-#' @param vars
+#' Note: this function accepts a sequence of indices as well, in this case it
+#' will return a matrix of assignments (one assignment per row).
 #'
-#' @return
+#' @param idx input index (assumed to be in correct range)
+#' @param vars a list of pairs, var index, var scope size
+#'
+#' @return the assignment associated to this index
+#'
 #' @export
 #'
 #' @examples
+#' vars <- list(c(1,3),c(3,2))
+#' index_to_assignment(3, vars)
+#' index_to_assignment(c(2,4,5), vars)
 index_to_assignment <- function(idx, vars) {
-
+  scope_sizes <- sapply(vars, pryr::f(x, x[2]))
+  strides <- c(1, cumprod(scope_sizes))
+  i2a <- pryr::f(i, ((idx - 1) %/% strides[i]) %% strides[i + 1] + 1)
+  sapply(seq_along(scope_sizes), i2a)
 }
 
-#' Title
+
+#' Convert assignent to incies
 #'
-#' @param assign
-#' @param vars
+#' Note: this function accepts a matrix of assignemnts (one per row), in this
+#' case the return will be a vector of respective indices.
 #'
-#' @return
+#' @param assign an assignment to the variables
+#' @param vars a list of paris, var index, var scope size
+#'
+#' @return the index associated to this assignment
 #' @export
 #'
 #' @examples
+#' vars <- list(c(1,3),c(3,2))
+#' assignment_to_index(c(1,1), vars)
+#' assignment_to_index(matrix(c(1,1, 3,2), nrow = 2, byrow = TRUE), vars)
 assignment_to_index <- function(assign, vars) {
-
+  scope_sizes <- sapply(vars, pryr::f(x, x[2]))
+  strides <- c(1, cumprod(head(scope_sizes, n = -1)))
+  as.vector((assign - 1) %*% strides + 1)
 }
 
 
-#' create_factor
+#' Create a factor
 #'
 #' @param vals the vector of values for this factor
 #' @param vars the list of variables and scopes for this factor.
--- tests/testthat/test_factors.R
+++ tests/testthat/test_factors.R
@@ -1,5 +1,21 @@
 context("general factor tests")
 
+test_that("check index_to_assignment and assignment_to_idex", {
+  expect_equal(index_to_assignment(1, list(c(2,1))),1)
+  N <- 111
+  for (idx in seq(1, N)) {
+    expect_equal(index_to_assignment(idx, list(c(2,N))),idx)
+  }
+  vars <- list(c(1,3),c(3,2))
+  assigns <- data.matrix(expand.grid(c(1,2,3), c(1,2)))
+  for (idx in seq(1, 2*3)) {
+    expect_equivalent(index_to_assignment(idx, vars), assigns[idx,])
+  }
+  expect_equivalent(index_to_assignment(seq(1, 2*3), vars), assigns)
+
+  expect_equivalent(assignment_to_index(assigns, vars), seq(1, 2*3))
+})
+
 test_that("factors test",{
   expect_error(create_factor(c(1,2), list(c(1,2), c(1,3))), regexp = "!anyDuplicated")
   expect_error(create_factor(c(1,2), list(c(1,2), c(2,3))), regexp = "prod")
